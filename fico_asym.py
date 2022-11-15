50ed332b1e7d345b39d9c9805cbf3179ee840b1a
Nicholas Wolczynski <nicholaswolczynski@Nicholass-MacBook-Pro.local>
2022-11-14 18:43:29 -0600

    fico 2 1 asym

--- fico_asym.py
+++ fico_asym.py
@@ -31,8 +31,8 @@ iters = 500
 coverage_reg = 0
 rejection_reg = 0
 fA=0.5
-asym_loss = [1.5,1]
-asym_accept = 0.5
+asym_loss = [2,1]
+asym_accept = 1
 
 
 #make teams
@@ -187,7 +187,7 @@ team_info.loc[3, 'human reject region train acc'] = metrics.accuracy_score(team3
 
 print(team_info)
 
-folder = 'fico_asym_15_1_results'
+folder = 'fico_asym_2_1_results'
 team_info.to_pickle('{}/start_info.pkl'.format(folder))
 
 team1.data_model_dict['Xtrain'].to_pickle('{}/startDataSet.pkl'.format(folder))
@@ -280,7 +280,7 @@ for run in range(0, 10):
         
         team1.set_training_params(Niteration, Nchain, Nlevel, Nrules, supp, maxlen, protected, budget, sample_ratio,
                                   alpha,
-                                  beta, iters, coverage_reg, contradiction_reg, fA, asym_loss = asym_loss)
+                                  beta, iters, coverage_reg, contradiction_reg, fA, asym_loss = asym_loss, asym_accept=0)
         team1.setup_hyrs()
         team1.train_hyrs()
         team1.filter_hyrs_results(mental=False, error=False)
@@ -310,7 +310,7 @@ for run in range(0, 10):
         print('training team3 hyrs model...')
         team3.set_training_params(Niteration, Nchain, Nlevel, Nrules, supp, maxlen, protected, budget, sample_ratio,
                                   alpha,
-                                  beta, iters, coverage_reg, contradiction_reg, fA, asym_loss = asym_loss)
+                                  beta, iters, coverage_reg, contradiction_reg, fA, asym_loss = asym_loss, asym_accept=0)
         team3.setup_hyrs()
         team3.train_hyrs()
         team3.filter_hyrs_results(mental=False, error=False)
