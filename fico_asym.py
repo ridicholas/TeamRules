92556f5a653b999a76fef3cfc4a9d1182e0f0691
Nicholas Wolczynski <nicholaswolczynski@Nicholass-MacBook-Pro.local>
2022-12-16 12:34:32 -0500

    made rand version run for 750

--- fico_asym.py
+++ fico_asym.py
@@ -203,7 +203,7 @@ for team in teams:
     i+=1
 print(team_info)
 
-folder = 'fico_asym_2_1_resultsMONTY'
+folder = 'fico_asym_2_1_resultsRAND'
 team_info.to_pickle('{}/start_info.pkl'.format(folder))
 
 team1.data_model_dict['Xtrain'].to_pickle('{}/startDataSet.pkl'.format(folder))
