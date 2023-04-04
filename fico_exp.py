682068ef0590585d084aa6b6c17052916e8fc4d2
Nicholas Wolczynski <nicholaswolczynski@Nicholass-MacBook-Pro.local>
2023-04-04 14:55:01 -0500

    det exps

--- fico_exp.py
+++ fico_exp.py
@@ -80,7 +80,7 @@ def complex_ADB(c_human, c_model, agreement, delta=5, beta=0.05, k=0.63, gamma=0
     return prob
 
 
-fA=complex_ADB
+fA=basic_ADB_func_det
 
 
 #make teams
@@ -199,7 +199,7 @@ team_info.loc[3, 'human reject region train acc'] = metrics.accuracy_score(team3
 
 print(team_info)
 
-folder = 'fico_contradiction_results_learned'
+folder = 'fico_contradiction_results_det'
 team_info.to_pickle('{}/start_info.pkl'.format(folder))
 
 team1.data_model_dict['Xtrain'].to_pickle('{}/startDataSet.pkl'.format(folder))
@@ -240,10 +240,10 @@ for run in range(0, 10):
 
     # train aversion and error boundary models
     team1.train_mental_aversion_model('perfect')
-    team1.train_confidence_model('xg', 0.2)
+    team1.train_confidence_model('perfect', 0.2)
     team1.train_mental_error_boundary_model()
-    team1.train_ADB_model(0.2)
-    team1.set_fA(team1.trained_ADB_model_wrapper)
+    #team1.train_ADB_model(0.2)
+    #team1.set_fA(team1.trained_ADB_model_wrapper)
     team_info.loc[1, 'human true accepts'] = (team1.data_model_dict['test_conf'] < team1_2_start_threshold).sum()
     team_info.loc[1, 'human true rejects'] = (team1.data_model_dict['test_conf'] >= team1_2_start_threshold).sum()
     team_info.loc[1, 'human accept region test acc'] = metrics.accuracy_score(
@@ -256,9 +256,9 @@ for run in range(0, 10):
                                                                          team1.data_model_dict['test_accept'])
 
     team2.train_mental_aversion_model('perfect')
-    team2.train_confidence_model('xg', 0.2)
-    team2.train_mental_error_boundary_model()
-    team2.train_ADB_model(0.2)
+    team2.train_confidence_model('perfect', 0.2)
+    #team2.train_mental_error_boundary_model()
+    #team2.train_ADB_model(0.2)
     team2.set_fA(team2.trained_ADB_model_wrapper)
     team_info.loc[2, 'human true accepts'] = (team2.data_model_dict['test_conf'] < team1_2_start_threshold).sum()
     team_info.loc[2, 'human true rejects'] = (team2.data_model_dict['test_conf'] >= team1_2_start_threshold).sum()
@@ -272,9 +272,9 @@ for run in range(0, 10):
                                                                          team2.data_model_dict['test_accept'])
 
     team3.train_mental_aversion_model('perfect')
-    team3.train_confidence_model('xg', 0.2)
-    team3.train_mental_error_boundary_model()
-    team3.train_ADB_model(0.2)
+    team3.train_confidence_model('perfect', 0.2)
+    #team3.train_mental_error_boundary_model()
+    #team3.train_ADB_model(0.2)
     team3.set_fA(team3.trained_ADB_model_wrapper)
     team_info.loc[3, 'human true accepts'] = (team3.data_model_dict['test_conf'] < team3_4_start_threshold).sum()
     team_info.loc[3, 'human true rejects'] = (team3.data_model_dict['test_conf'] >= team3_4_start_threshold).sum()
