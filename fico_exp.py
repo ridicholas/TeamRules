cda045649129fe22a3f22864b5390cc414d14012
Nicholas Wolczynski <nicholaswolczynski@Nicholass-MacBook-Pro.local>
2023-05-03 12:28:29 -0500

    len3 perfect

--- fico_exp.py
+++ fico_exp.py
@@ -210,7 +210,7 @@ team_info.loc[3, 'human reject region train acc'] = metrics.accuracy_score(team3
 
 print(team_info)
 
-folder = 'fico_contradiction_results_learned_len3'
+folder = 'fico_contradiction_results_perfect_len3'
 team_info.to_pickle('{}/start_info.pkl'.format(folder))
 
 team1.data_model_dict['Xtrain'].to_pickle('{}/startDataSet.pkl'.format(folder))
@@ -265,10 +265,10 @@ for run in range(0, 10):
     # train aversion and error boundary models
     if 'team1' in whichTeams:
         team1.train_mental_aversion_model('perfect')
-        team1.train_confidence_model('not_perfect', 0.2)
+        team1.train_confidence_model('perfect', 0.2)
         team1.train_mental_error_boundary_model()
-        team1.train_ADB_model(0.2)
-        team1.set_fA(team1.trained_ADB_model_wrapper)
+        #team1.train_ADB_model(0.2)
+        #team1.set_fA(team1.trained_ADB_model_wrapper)
         team_info.loc[1, 'human true accepts'] = (team1.data_model_dict['test_conf'] < team1_2_start_threshold).sum()
         team_info.loc[1, 'human true rejects'] = (team1.data_model_dict['test_conf'] >= team1_2_start_threshold).sum()
         team_info.loc[1, 'human accept region test acc'] = metrics.accuracy_score(
@@ -282,10 +282,10 @@ for run in range(0, 10):
 
     if 'team2' in whichTeams:
         team2.train_mental_aversion_model('perfect')
-        team2.train_confidence_model('not_perfect', 0.2)
+        team2.train_confidence_model('perfect', 0.2)
         team2.train_mental_error_boundary_model()
-        team2.train_ADB_model(0.2)
-        team2.set_fA(team2.trained_ADB_model_wrapper)
+        #team2.train_ADB_model(0.2)
+        #team2.set_fA(team2.trained_ADB_model_wrapper)
         team_info.loc[2, 'human true accepts'] = (team2.data_model_dict['test_conf'] < team1_2_start_threshold).sum()
         team_info.loc[2, 'human true rejects'] = (team2.data_model_dict['test_conf'] >= team1_2_start_threshold).sum()
         team_info.loc[2, 'human accept region test acc'] = metrics.accuracy_score(
@@ -299,10 +299,10 @@ for run in range(0, 10):
 
     if 'team3' in whichTeams:
         team3.train_mental_aversion_model('perfect')
-        team3.train_confidence_model('not_perfect', 0.2)
+        team3.train_confidence_model('perfect', 0.2)
         team3.train_mental_error_boundary_model()
-        team3.train_ADB_model(0.2)
-        team3.set_fA(team3.trained_ADB_model_wrapper)
+        #team3.train_ADB_model(0.2)
+        #team3.set_fA(team3.trained_ADB_model_wrapper)
         team_info.loc[3, 'human true accepts'] = (team3.data_model_dict['test_conf'] < team3_4_start_threshold).sum()
         team_info.loc[3, 'human true rejects'] = (team3.data_model_dict['test_conf'] >= team3_4_start_threshold).sum()
         team_info.loc[3, 'human accept region test acc'] = metrics.accuracy_score(
