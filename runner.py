0c80ba8f4df3296fe8206e6f91d2696fa36edef2
Nicholas Wolczynski <nicholaswolczynski@Nicholass-MacBook-Pro.local>
2023-10-20 15:41:02 -0500

    timing

--- runner.py
+++ runner.py
@@ -68,12 +68,12 @@ def run(team1, team2, team3, folder, team_info):
     team3_4_start_threshold = 0.5
 
     #initial hyperparams
-    Niteration = 1000
+    Niteration = 50
     Nchain = 1
     Nlevel = 1
     Nrules = 10000
     supp = 5
-    maxlen = 4
+    maxlen = 2
     protected = 'NA'
     budget = 1
     sample_ratio = 1
